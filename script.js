f9776cd767fa48e3dd7e4e13af29a515a036e6c7
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-09-08 19:19:59 +0530

    crearing the bugs

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+
+
+
 let canvas;
 let canvasContext;
 let ballPositionX = 50;
 let ballSpeedX = 10;
+let ballPositionY=50
+let ballSpeedY=4;
+let paddle1=250;
+const PADDLE_HEIGHT=100;
 
 window.onload=function () {
     canvas = document.getElementById('game');
     canvasContext=canvas.getContext('2d');
-}
+    let Speed = 30;
+    setInterval(function callBoth() {
+        ballMovement();
+        drawcanvas();
+        
+    },1000/Speed)
 
+
+    canvas.addEventListener('mousemove',function(x){
+        let mouPosition=mousePosition(x)
+        paddle1=mouPosition.y;
+    })
+
+}
 function ballMovement() {
     ballPositionX = ballPositionX + ballSpeedX;
     if(ballPositionX > canvas.width){
         ballSpeedX = -ballSpeedX
     }
-    
+    if(ballPositionX < 0){
+        ballSpeedX = -ballSpeedX
+    }
+
+
     ballPositionY = ballPositionY+ ballSpeedY;
     if(ballPositionY > canvas.height){
         ballSpeedY = -ballSpeedY
@@ -21,8 +44,12 @@ function ballMovement() {
     if(ballPositionY < 0){
         ballSpeedY = -ballSpeedY
     }
+    
 }
-
+ function drawElement(x,y,w,h,color){
+    canvasContext.fillStyle=color;
+    canvasContext.fillRect(x,y,w,h);
+ }
 
 function drawcanvas () {
     
@@ -32,14 +59,6 @@ function drawcanvas () {
     drawElement(ballPositionX,ballPositionY,10,10,'red');
 }
 
-
-function drawElement(x,y,w,h,color){
-    canvasContext.fillStyle=color;
-    canvasContext.fillRect(x,y,w,h);
- }
-
-
- 
 function mousePosition(x){
     let can=canvas.getBoundingClientRect();
     let root=document.documentElement;
